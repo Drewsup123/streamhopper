0d6a03b4e953c8085eb460b50f4f401d7b1c49c7
Drew Johnson <drewsup123c@gmail.com>
2022-07-23 20:18:45 -0600

    Added basic search screen

--- src/pages/Home.page.tsx
+++ src/pages/Home.page.tsx
@@ -1,12 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
 
 const Home = () => {
+    const [search, setSearch] = useState<string>("");
+
+    const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        e.preventDefault();
+        e.stopPropagation();
+        setSearch(e.target.value);
+    }
+
     return (
         <div className="homepage">
             <div id="wave-animation">
                 <div className="drop"></div>
                 <div className="wave"></div>
             </div>
+            <input 
+                className="homepage-search-input"
+                value={search}
+                onChange={handleSearchChange}
+                placeholder="Search for any movie or tv show..."
+            />
         </div>
     );
 };
