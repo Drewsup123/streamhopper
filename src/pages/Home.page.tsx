9e5f668ab2f36b88b0cf4b6cb2d7fd542e8d5976
Drew <drewsup123c@gmail.com>
2022-07-23 19:11:37 -0600

    boilerplate homepage

---
+++ src/pages/Home.page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <div className="homepage">
+            <div className="drop"></div>
+            <div className="wave"></div>
+        </div>
+    );
+};
+
+export default Home;
